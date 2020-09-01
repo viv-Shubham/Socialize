0e064903fd372091776067ee8d3ee1969669b9ab
Shubham Aggarwal <viv.shubham077@gmail.com>
2020-09-01 14:49:59 +0530

    make it better

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
 app.listen(PORT,()=>{
-    console.log("server has started on port 5000");
+    console.log("server has started on",PORT);
 })
